084b38845a7ac9a0864b3b5a331d5260791cb776
rmcool26@gmail.com <rmcool26@gmail.com>
2026-05-24 17:29:01 +0530

    Siteguard enable karna bhul gaya tha

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      {/* <SiteGuards /> */}
+      <SiteGuards />
       <Toaster />
       <Sonner />
       <BrowserRouter>
